6583af7e96f6881186ef87ee7aac1d29a7a056ee
lalit93 <lalit93chachlani@gmail.com>
2025-07-28 00:03:40 +0530

    Fix for using WIF instead of ADC

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "google" {
   project = "gcp-tf-git"
   region  = "us-central1"
+  # Enable ADC through GitHub Actions WIF
+  credentials = null
 }
+
 
 resource "google_storage_bucket" "test_bucket" {
   name     = "lalit-terraform-test-bucket"
