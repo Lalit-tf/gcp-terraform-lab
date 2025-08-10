294800325c310c2c0fbd46dbd2415c39e451351d
lalit93 <tsungdalar@gmail.com>
2025-08-10 14:47:05 +0530

    Adding remote backend for storing state

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
+# Define the Google Cloud provider
 provider "google" {
   project = "gcp-tf-git"
   region  = "us-central1"
-  #use WIF instead of ADC as per configured in Action Secret
 }
 
-
-resource "google_storage_bucket" "test_bucket" {
-  name     = "lalit-terraform-test-bucket"
+# Create a GCS bucket for Terraform state
+resource "google_storage_bucket" "terraform_state" {
+  name     = "remote backend"  # Ensure this is globally unique
   location = "US"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Now, we’ll reference this bucket for remote state storage
+terraform {
+  backend "gcs" {
+    bucket = "remote backend"  #  references the created bucket
+    prefix = "terraform/state"  # This creates a folder structure inside the bucket
+  }
 }
